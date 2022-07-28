67040c07cf4b6ec4fac71c2a5990726c1a436d44
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2022-07-28 15:07:53 +1200

    Cleanup

--- imagedelivery.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-url = sys.argv[1]
-url = url.replace("imagedelivery.net", "divamodarchive.xyz/cdn-cgi/imagedelivery")
-print(url)
--- watch.sh
+++ watch.sh
@@ -1,1 +1,1 @@
-cargo watch -x run -i storage -i src/api/v1.json -i migrations -i target -i templates -i .gitignore -i rustfmt.toml -i setcap.sh -i test_data.sql -i watch.sh
+cargo watch -x run -i storage -i src/api/v1.json -i migrations -i target -i templates -i .gitignore -i rustfmt.toml -i setcap.sh -i watch.sh
